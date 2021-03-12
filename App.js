f9dd2d7c4d9208141009a54ca2afb84f4b7c9e61
Marcelo Santos <rocha.mer21@gmail.com>
2021-03-12 14:39:38 -0300

    AuthStack created, initiate PreloadScreen

--- App.js
+++ App.js
@@ -1,14 +1,16 @@
 import React from 'react'
-import {View, Text} from 'react-native'
+import { NavigationContainer } from '@react-navigation/native'
 
-import {StateProvider} from './src/contexts/StateContext'
+import { StateProvider } from './src/contexts/StateContext'
+
+import AuthStack from './src/stacks/AuthStack'
 
 export default () => {
   return (
     <StateProvider>
-      <View>
-        <Text>Estou funcionando 2</Text>
-      </View>
+      <NavigationContainer>
+        <AuthStack />
+      </NavigationContainer>
     </StateProvider>
   )
 }
